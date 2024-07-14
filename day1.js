f13dca7aadb7c5dfe896e48639df7befcbe8eb1b
Awatans <theavi0509@gmail.com>
2024-07-14 09:35:50 +0530

    day 1

---
+++ day1.js
@@ -0,0 +1,39 @@
+//activity 1
+//task 1
+var a = 45;
+console.log(a);
+//task 2
+let b = "hitsh is god";
+console.log(b);
+
+//activity 2
+//task 3
+const c = true
+console.log(c);
+
+//activity 3
+//task 4
+let x = 5;
+console.log(typeof x);
+let y = "saurav";
+console.log(typeof y);
+let z = true || false
+console.log(typeof z);
+let obj = {};
+console.log(typeof obj);
+let arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+//activity 4
+//task 5
+let value = "hitsh";
+console.log(value);
+value = "hitsh chaudhary";
+console.log(value);
+
+//activity 5
+//task 6
+const theme = "dark";
+theme = "white";
+console.log(theme);
+//error: is assignment to constant variable
