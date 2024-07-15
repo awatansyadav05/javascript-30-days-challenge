3cde08e9480aa84a72fd9a52d7894452aab218cb
Awatans <theavi0509@gmail.com>
2024-07-15 21:52:40 +0530

    day 2 operators

---
+++ day2.js
@@ -0,0 +1,64 @@
+//operators
+//activity 1: Airthemtic operators
+//task 1
+let a = 54;
+let b = 45;
+let ans = a + b;
+console.log(ans);
+
+//task 2
+ans = a - b;
+console.log(ans);
+
+//task 3
+multi = a * b;
+console.log(ans);
+
+//task 4
+div = a / b;
+console.log(ans);
+
+modulo = a % b;
+console.log(ans);
+
+//activity 2: Assignment operators
+//task 6
+ans += a;
+console.log(ans);
+//task 7
+ans -= a;
+console.log(ans);
+
+//activity 3: comaprison operators
+//task 8
+ans=a>b;
+console.log(ans);
+
+//task 9
+ans=a<b;
+console.log(ans);
+
+//task 10
+ans = a==b;
+console.log(ans);
+
+ans=a===b;
+console.log(ans);
+
+//activity 4: logical operators
+//task 11
+ans = (a>=48 && b<= 56 );
+console.log(ans);
+
+//task 12
+ans=(a>48 || b<=56) ;
+console.log(ans);
+
+//task 13
+ans= !(a>48 || b<=56)
+console.log(ans);
+
+//activity 4: ternany operators
+
+ans= a>=0? 
+console.log("positive") : console.log("negative");;
